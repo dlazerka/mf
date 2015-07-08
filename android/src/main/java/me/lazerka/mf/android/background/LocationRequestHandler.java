28c7008b68beca175dfe0c756e64a59c459f5d84
Dzmitry Lazerka <dlazerka@gmail.com>
2015-07-08 12:46:04 -0700

    Added friend-check on location request.

--- android/src/main/java/me/lazerka/mf/android/background/LocationRequestHandler.java
+++ android/src/main/java/me/lazerka/mf/android/background/LocationRequestHandler.java
@@ -4,12 +4,15 @@ import android.content.Context;
 import android.location.LocationManager;
 import android.os.Message;
 import android.os.SystemClock;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import android.widget.Toast;
 import com.android.volley.Request.Method;
 import com.android.volley.VolleyError;
 import me.lazerka.mf.android.Application;
 import me.lazerka.mf.android.R;
+import me.lazerka.mf.android.adapter.FriendInfo;
+import me.lazerka.mf.android.adapter.FriendsLoader;
 import me.lazerka.mf.android.http.JsonRequester;
 import me.lazerka.mf.api.gcm.LocationRequestGcmPayload;
 import me.lazerka.mf.api.object.Location;
@@ -20,6 +23,7 @@ import org.joda.time.DateTime;
 
 import java.io.IOException;
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 
 import static org.joda.time.DateTimeZone.UTC;
 
@@ -44,6 +48,7 @@ public class LocationRequestHandler extends GcmMessageHandler<LocationRequestGcm
 
 		GcmIntentService service = serviceRef.get();
 		if (service == null) {
+			// Probably happens when device is turning off.
 			String msg = "Service is GCed, not sending my location";
 			Log.e(TAG, msg);
 			ACRA.getErrorReporter().handleException(new RuntimeException(TAG + ": " + msg));
@@ -59,6 +64,20 @@ public class LocationRequestHandler extends GcmMessageHandler<LocationRequestGcm
 		}
 
 		String requesterEmail = payload.getRequesterEmail();
+
+		// Not ideally clean to use FriendsLoader this way, but clean enough for now.
+		FriendInfo friend = getFriendInfo(requesterEmail, service);
+		if (friend == null) {
+			Log.w(TAG, "Requester not in friends list, rejecting " + requesterEmail);
+
+			String appName = service.getResources().getString(R.string.app_name);
+			sendNotification(
+					requesterEmail + " requested your location, but not in friends list, rejected",
+					appName,
+					service);
+
+			return;
+		}
 
 		String appName = service.getResources().getString(R.string.app_name);
 		sendNotification(requesterEmail + " requested your location", appName, service);
@@ -85,6 +104,22 @@ public class LocationRequestHandler extends GcmMessageHandler<LocationRequestGcm
 				.show();
 			Log.e(TAG, "No lastKnownLocation");
 		}
+	}
+
+	@Nullable
+	private FriendInfo getFriendInfo(String requesterEmail, GcmIntentService service) {
+		FriendsLoader friendsLoader = new FriendsLoader(service);
+		ArrayList<FriendInfo> friendInfos = friendsLoader.loadInBackground();
+
+		FriendInfo friend = null;
+		for(FriendInfo friendInfo : friendInfos) {
+			// TODO check normalized
+			if (friendInfo.emails.contains(requesterEmail)) {
+				friend = friendInfo;
+				break;
+			}
+		}
+		return friend;
 	}
 
 	private class LocationSender extends JsonRequester<MyLocation, MyLocationResponse> {
